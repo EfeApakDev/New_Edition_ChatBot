f33e62f31b278ac8002dc9b68b88266b02d76766
SakirBey1 <sakirhack81@gmail.com>
2022-07-10 14:07:22 +0300

    Delete Config.py

--- Config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-class Config():
-    # Get these values from my.telegram.org
-    # https://my.telegram.org
-    BOT_ID = int(os.environ.get("BOT_ID"))
-    OWNER_ID = int(os.environ.get("OWNER_ID"))
